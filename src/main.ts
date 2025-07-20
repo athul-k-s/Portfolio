e043281092b2f50ec82f7985311cd7966aa6d9c4
athul <athul.ks@opsotech.com>
2025-07-20 15:30:18 +0530

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { provideRouter } from '@angular/router';
+import { provideZoneChangeDetection } from '@angular/core';
+import { routes } from './app/app.routes';
 
-bootstrapApplication(App, appConfig)
+
+bootstrapApplication(App, {
+  ...appConfig,
+  providers: [
+    provideRouter(routes),
+    provideZoneChangeDetection({ eventCoalescing: true }) // Zone-less config
+  ]
+})
   .catch((err) => console.error(err));
