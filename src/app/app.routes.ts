37d87e26894b605d222c33c69e54e469ceb257a8
athul <athul.ks@opsotech.com>
2025-07-20 13:35:16 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { Layout } from './Components/layout/layout';
+import { Index } from './Components/index';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Layout,
+        children: [
+            {
+                path: '',
+                pathMatch: 'full',
+                component: Index
+            },
+            {
+                path: 'index',
+                component: Index
+            }
+        ]
+    }
+];
